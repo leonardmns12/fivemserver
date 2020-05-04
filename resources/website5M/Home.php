c92fe4ba8db921e3142231fa1010e408b571834a
Nico Fernando <nicofernando39@gmail.com>
2020-05-04 22:07:58 +0700

    web
    as

--- resources/website5M/Home.php
+++ resources/website5M/Home.php
@@ -21,35 +21,74 @@
     <div style="display:flex; flex-direction:column"> 
 
       <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="logo">
+                <a href="Home.php"><img src="image/IFR2.png" alt="IFR"></a>
+            </div>
+            <span class="text">
+                <p>
+                    Welcome, 
+                </p>
+            </span>
         <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
+          <div class="navbar-nav"> -->
             <!-- <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a> -->
-            <!-- <a class="nav-item nav-link" href="#">Features</a> -->
+            <!-- <a class="nav-item nav-link" href="#">Features</a>
             <button class="btn nav-link navbar-button" data-toggle="modal" data-target="#Loginmodal">Login</button>
-            <button class="btn nav-link navbar-button" data-toggle="modal" data-target="#RegisterModal">Register</button>
-          </div>
-        </div>
+            <button class="btn nav-link navbar-button" data-toggle="modal" data-target="#RegisterModal">Register</button> -->
+            
+          <!-- </div>
+        </div> -->
       </nav>
-
-
       
       <div class="row">
         <div class="col-xs-2 search-input-position" style="display:flex;">
-          <input type="text" class="form-control search-input" placeholder="Search">
+          <input type="text" class="form-control search-input" placeholder="Coupon">
             <div class="input-group-btn">
               <button class="btn btn-default btn-input" type="submit">
-                <i class="fas fa-search"></i>
+                  <img src="image/coupon.png" alt="coupon" style="height:30px;">
+                <!-- <i class="fas fa-search"></i> -->
               </button>
           </div>
       </div>
       
     </div>
       <div class="container-fluid">
-        <!-- <img src="image/IFR.png" alt="IFR" style="width:97%;"> -->
+        <div class="download_launcher">
+            <button class="btn btn-default download">
+                Download Launcher
+                <img src="image/interface.png" alt="interface" style="height:40px; width:40px; margin-bottom:10px">
+            </button>
+        </div>
+        <div class="wallet">
+            <button class="btn btn-default wallets">
+                <img src="image/wallet.png" alt="wallet" style="height:30px; width:30px">
+                <span class="money">
+                    <p>
+                        150000
+                    </p>
+                </span>
+            </button>
+        </div>
+        <div class="power">
+            <a href="#"><img src="image/logout.png" alt="Logout" style="height:40px; width:40px"></a>
+        </div>
+        <div class="connect">
+            <span class="hai">
+                <p id="a">
+                    You are  
+                </p>
+                <p style="color: #36A86B;" id="a1">
+                    connected
+                </p>
+                <p id="to">
+                    to FiveM
+                </p>
+            </span>
+        </div>
       </div>
       <!-- Button trigger modal -->
 
@@ -64,7 +103,7 @@
                 <span aria-hidden="true" style="color: #FFF;">&times;</span>
               </button>
             </div>
-            <form action="/website5m/Home.php">
+            <form action="/index.html">
               <div class="modal-body">
                     <div class="input-group mb-3">
                       <div class="input-group-prepend">
