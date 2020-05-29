32716fb6c999b654274c20e7e6e7887af985e23f
leonardmns12 <54368620+leonardmns12@users.noreply.github.com>
2020-05-29 19:10:19 +0700

    clear caches

--- resources/[multichar]/esx_kashacter/html/locales/en.js
+++ resources/[multichar]/esx_kashacter/html/locales/en.js
@@ -1,12 +1,12 @@
 const translate = new Object();
 
-translate.job = "Job";
+translate.job = "Pekerjaan";
 translate.bank = "Bank";
 translate.money = "Cash";
 translate.gender = "Gender";
-translate.dob = "Date of birth";
-translate.new = "Create new character";
-translate.delete = "DELETE";
-translate.play = "PLAY";
+translate.dob = "Tanggal Lahir";
+translate.new = "Buat karakter baru";
+translate.delete = "HAPUS";
+translate.play = "MULAI";
 translate.modalTitle = "Are You Sure?";
 translate.modalText = "By deleting your account we won't be able to recover it, all properties, cars etc. will be deleted";
